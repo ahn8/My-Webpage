3d99b2e25233a1260e9dee30c3392253a8f17dad
ahn8 <114027882+ahn8@users.noreply.github.com>
2022-11-29 12:43:36 -0500

    Add files via upload

---
+++ Iphone 14, Iphone 14 Pro.cs
@@ -0,0 +1,424 @@
+Iphone 14, Iphone 14 Pro
+$799.99, $999.99 $1,099.99
+Image, Link 
+Triple 12MP Ultra Wide + Wide + Telephoto
+12MP TrueDepth Front Camera
+23 Hours talk time
+N/A standby time
+2556 x 1179 pixels
+6.1-inch Super Retina XDR Display with ProMotion
+Dynamic Island
+48MP Main camera
+Cinematic mode
+Action mode for smooth, steady, handheld videos
+Emergency SOS via satellite³
+All-day battery life
+Splash, Water, and Dust Resistant
+iOS 16
+5G Connectivity 
+
+Iphone 14 Pro Max
+$1,099.99
+Image, Link
+Triple 12MP Ultra Wide + Wide + Telephoto
+12MP TrueDepth Front Camera
+29 Hours talk time
+N/A standby time
+6.7-inch Super Retina XDR Display with ProMotion
+2796 x 1290 pixels
+Other features
+6.7-inch Super Retina XDR Display with ProMotion
+Dynamic Island
+48MP Main camera
+Cinematic mode
+Action mode for smooth, steady, handheld videos
+Emergency SOS via satellite³
+All-day battery life
+Splash, Water, and Dust Resistant
+iOS 16
+5G Connectivity
+
+IPhone 14 Plus 
+$899.99
+Image, Link
+Dual 12MP Wide & Ultra Wide Cameras
+12MP TrueDepth Front Camera
+20 Hours talk time
+N/A standby time
+6.7-inch Super Retina XDR display
+2778 x 1284 pixels
+6.7-inch Super Retina XDR display¹
+Advanced Dual-Camera System
+Cinematic Mode
+Emergency SOS via satellite³
+All-day battery life
+Splash, Water, and Dust Resistant
+iOS 16
+5G Connectivity
+4K Dolby Vision HDR Recording
+Up to 26 Hours of Video Playback
+
+Pixel 7
+$599.99
+Image, Link
+50 MP wide lens + 12 MP 4K
+10.8 MP Front Camera
+N/A talk time
+N/A standby time
+6.3 inches QHD+ LTPO Smooth Display
+2400 x 1080 pixels
+Wide and ultrawide lenses with up to 8x Super Res Zoom²
+6.3" FHD+ Smooth Display up to 90Hz¹
+Powered by Google Tensor G2 and Titan M2 security chip
+Cinematic Blur brings more drama to your videos
+Adaptive Battery lasts over 24 hours³
+With Extreme Battery Saver - up to 72 hours battery life.⁴
+Make distractions disappear, with Magic Eraser
+Real tone - The camera that makes you feel seen
+Photo Unblur - fix your blurry photos⁵
+Translate languages in real time with Live Translate.
+
+Pixel 7 Pro
+$899.99
+Image, Link
+48 MP telephoto lens + 50 MP wide lens + 12 MP 4K
+10.8 MP Front Camera
+N/A talk time
+N/A standby time
+6.7 inches QHD+ LTPO Smooth Display
+3120 x 1440 pixels
+6.7" QHD+ LTPO Smooth Display up to 120Hz¹
+5x telephoto lens and 30x Super Res Zoom²
+Powered by Google Tensor G2 & Titan M2 security chip
+Pro-level triple camera system
+Macro Focus powered by ultrawide lens & autofocus
+Adaptive Battery lasts over 24 hours³
+Turn on Extreme Battery Saver, and the battery can last up to 72 hours.⁴
+Make distractions disappear, with Magic Eraser.
+Photo Unblur - fix your blurry photos.⁵
+Translate languages in real time with Live Translate.
+
+Galazy S22+
+$999.99
+Image, Link
+50MP(Wide) + 10MP(Tele) + 12MP(Ultra-Wide) Rear Cameras
+10MP Front Camera
+36 Hours talk time
+11.5 Days standby time
+6.6” FHD+ Dynamic AMOLED-2x Flat Display
+2340 x 1080 pixels
+50MP(Wide) + 10MP(Tele) + 12MP(Ultra-Wide) Rear Cameras
+10MP Front Camera
+8K Video
+Super-steady Video
+Night Mode
+Triple-Lens Camera with Portrait Mode
+30x Space Zoom
+Single Take
+Director’s View
+Bright Display with Vision Booster
+Adaptive Color Contrast
+Fast Qualcomm Snapdragon 8 Gen 1
+Long Lasting 4500 mAh Battery
+5G Capable
+Super-Fast Charging (Power Adapter sold separately)
+Premium Design
+Live Sharing with Google Duo (Co-Watching)
+Bluetooth 5.2
+
+Galazy S22
+$799.99
+50MP(Wide) + 10MP(Tele) + 12MP(Ultra-Wide) Rear Cameras
+10MP Front Camera
+29 Hours talk time
+10.8 Days standby time
+6.1” FHD+ Dynamic AMOLED-2x Flat Display
+2340 x 1080 pixels
+8K Video
+Super-steady Video
+Night Mode
+Triple-Lens Camera with Portrait Mode
+30x Space Zoom
+Single Take
+Director’s View
+Bright Display with Vision Booster
+Adaptive Color Contrast
+Fast Qualcomm Snapdragon 8 Gen 1
+Long Lasting 3700 mAh Battery
+5G Capable
+Supports Super-Fast Charging
+Premium Design
+Live Sharing with Google Duo (Co-Watching)
+Bluetooth 5.2
+
+Galaxy S22 Ultra
+$1,199.99 
+Image Link
+108MP(Wide)+2x10MP(Tele)+12MP(Ultra-Wide) Rear Cameras
+40MP Front Camera
+40 Hours talk time
+12.01 Days standby time
+6.8” WQHD+ Dynamic AMOLED-2x Curved Display
+3080 x 1440 pixels
+8K Video
+Super-steady Video
+Embedded SPen
+Night Mode
+Quad Camera with 108MP Main Lens
+100x Space Zoom
+Single Take
+Director’s View
+Bright Display with Vision Booster
+Adaptive Color Contrast
+Fast Qualcomm Snapdragon 8 Gen 1
+Long Lasting 5000 mAh Battery
+5G Capable
+Super-Fast Charging (Power Adapter sold separately)
+Premium Design
+Live Sharing with Google Duo (Co-Watching)
+Bluetooth 5.2
+
+PCS/Consoles
+
+Dell Inspirion 3511
+$399.99
+Image Link
+Screen Size
+15.6 inches
+Screen Resolution
+1920 x 1080 (Full HD)
+Touch Screen
+Processor Model
+Intel 10th Generation Core i5
+Processor Model Number
+1035G1
+Processor Speed (up to)
+1 gigahertz
+Storage Type- SSD
+Total Storage Capacity
+256 gigabytes
+Solid State Drive Capacity
+256 gigabytes
+System Memory (RAM)
+8 gigabytes
+Graphics
+Intel UHD Graphics
+Operating System
+Windows 11 Home in S Mode
+Battery Type
+Lithium-ion
+
+Macbook Air 
+$1,049.99
+Image Link
+Screen Size
+13.6 inches
+Screen Resolution
+2560 x 1664
+Processor Model
+Apple M2
+Processor Model Number
+Apple M2
+Storage Type
+SSD
+Total Storage Capacity
+512 gigabytes
+Solid State Drive Capacity
+512 gigabytes
+System Memory (RAM)
+8 gigabytes
+Graphics
+Apple M2 10-core
+Operating System
+macOS Monterey 12
+Battery Type
+Lithium-polymer
+Backlit Keyboard
+Yes
+
+Microsoft Surface Pro 9
+$1,599.99
+Image Link
+Screen Size
+13 inches
+Screen Resolution
+2880 x 1920
+Processor Model
+12th Gen Intel Core i7-1255U
+Total Storage Capacity
+256 gigabytes
+Operating System
+Windows 11 Home
+Wireless Connectivity
+Bluetooth, Wi-Fi
+Battery Type
+Lithium-ion
+2-in-1 Design
+Yes
+Security Features
+Facial recognition
+
+Dell XPS 15
+$2,099.99
+Image Link
+Screen Size
+15.6 inches
+Screen Resolution
+1920 x 1200 (WUXGA)
+Touch Screen
+No
+Processor Model
+Intel 12th Generation Core i7
+Processor Model Number
+12700H
+Storage Type
+SSD
+Total Storage Capacity
+512 gigabytes
+Solid State Drive Capacity
+512 gigabytes
+System Memory (RAM)
+16 gigabytes
+Graphics
+NVIDIA GeForce RTX 3050 Ti
+Operating System
+Windows 11 Home
+Voice Assistant Built-in
+Amazon Alexa
+Works With
+Amazon Alexa
+Battery Type
+Lithium-ion
+2-in-1 Design
+No
+Backlit Keyboard
+Yes
+
+Mackbook Pro 16
+$3,499.99
+Screen Size
+16.2 inches
+Screen Resolution
+3456 x 2234
+Touch Screen
+No
+Processor Model
+Apple M1 Max
+Processor Model Number
+Apple M1 Max
+Storage Type
+SSD
+Total Storage Capacity
+1000 gigabytes
+Solid State Drive Capacity
+1000 gigabytes
+System Memory (RAM)
+32 gigabytes
+Graphics
+Apple M1 Max 32-core
+Operating System
+macOS Monterey 12
+Voice Assistant Built-in
+Siri
+Battery Life (up to)
+21 hours
+Battery Type
+Lithium-polymer
+Backlit Keyboard
+Yes
+
+IPad Pro
+$1,399.99
+Screen Size
+12.9 inches
+Screen Resolution
+2732 x 2048
+Processor Model
+Apple M2 chip with 8-core CPU
+Total Storage Capacity
+256 gigabytes
+Operating System
+Apple iOS 12.2
+Works With
+Siri
+Wireless Connectivity
+Bluetooth, Wi-Fi
+Battery Type
+Lithium-ion
+Security Features
+Touch ID sensor
+
+Microsoft Surface Pro 8
+$899.99
+Image Link
+Screen Size
+13 inches
+Screen Resolution
+2880 x 1920
+Processor Model
+Intel 11th Gen Core i5-1135G7
+Total Storage Capacity
+256 gigabytes
+Operating System
+Windows 11 Home
+Wireless Connectivity
+Bluetooth, Wi-Fi
+Battery Type
+Lithium-ion
+2-in-1 Design
+Yes
+Security Features
+Facial recognition
+
+IPad 10th Gen
+$499.99
+Image Link
+Screen Size
+10.9 inches
+Screen Resolution
+2360 x 1640
+Processor Model
+A14 Bionic chip
+Total Storage Capacity
+64 gigabytes
+Operating System
+Apple iPadOS
+Wireless Connectivity
+Bluetooth, Wi-Fi
+Battery Type
+Lithium-polymer
+Security Features
+Touch ID sensor
+
+Xbox Series S
+$299.99
+Surround Sound Supported
+Dolby Digital, Dolby TrueHD, DTS
+High Dynamic Range (HDR)
+Yes
+Storage Capacity
+512 gigabytes
+Number Of USB Port(s)
+3
+Number of HDMI Outputs (Total)
+1
+
+Xbox Series X 
+$499.99
+Surround Sound Supported
+Dolby Digital, Dolby TrueHD, DTS
+Maximum Graphic Quality
+2160p (4K)
+High Dynamic Range (HDR)
+Yes
+Storage Capacity
+1000 gigabytes
+Number Of USB Port(s)
+3
+Number of HDMI Outputs (Total)
+1
+Number Of Optical Digital Audio Outputs
+0
+
+
+
